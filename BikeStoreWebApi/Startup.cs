b39d1ec804c924685261fc68892643239a0d7841
Oleksandr Dovbnia <sashadovbnya1@gmail.com>
2023-02-25 23:34:09 +0200

    Added app insights.

--- BikeStoreWebApi/Startup.cs
+++ BikeStoreWebApi/Startup.cs
@@ -68,6 +68,7 @@ namespace BikeStoreWebApi
                 }).AddEntityFrameworkStores<BikeStoreDBContext>();
 
             services.ConfigureAuthenticationService(Configuration.GetSection("Auth"));
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
